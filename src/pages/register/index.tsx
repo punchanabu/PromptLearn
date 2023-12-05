e0d74c7ec5d6168f392528252c46929aaeb7a617
Sirawit Chanaburanasak <97283645+punchanabu@users.noreply.github.com>
2023-12-06 02:22:11 +0700

    Update index.tsx

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -30,7 +30,7 @@ const Register = () => {
         }
 
         try {
-            const response = await fetch('http://localhost:3000/api/auth/register', {
+            const response = await fetch('/api/auth/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
